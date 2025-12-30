dbae850f5afbc70b2ef50b8173fc613f6fc8fe07
VhRvo <zyzhuo@outlook.com>
2025-12-30 14:38:44 +0800

    .

--- src/lc740.rs
+++ src/lc740.rs
@@ -14,20 +14,41 @@ impl Solution {
             }
             num_counts
         };
+        // num_counts[num_counts.len()].0 as a dummy value, which is not equal to any valid number + 1
         let mut dp = vec![0; num_counts.len() + 1];
         dp[num_counts.len()] = 0;
         for i in (0..num_counts.len()).rev() {
             dp[i] = dp[i + 1];
-            for j in i + 1..=num_counts.len() {
-                if j == num_counts.len() {
-                    dp[i] = dp[i].max(num_counts[i].0 * num_counts[i].1);
-                    break;
-                }
-                if num_counts[j].0 != num_counts[i].0 + 1 {
-                    dp[i] = dp[i].max(num_counts[i].0 * num_counts[i].1 + dp[j]);
-                    break;
-                }
-            }
+
+            // Find the next selectable position (first position where the number is not current number + 1)
+            // let next_pos = {
+            //     let mut j = i + 1;
+            //     while j < num_counts.len() && num_counts[j].0 == num_counts[i].0 + 1 {
+            //         j += 1;
+            //     }
+            //     j
+            // };
+
+            // Or use simple loop
+            // let mut next_pos = num_counts.len();
+            // for j in i + 1..num_counts.len() {
+            //     if num_counts[j].0 != num_counts[i].0 + 1 {
+            //         next_pos = j;
+            //         break;
+            //     }
+            // }
+
+            // Or use iterator
+            // let next_pos = (i + 1..num_counts.len())
+            //     .find(|&j| num_counts[j].0 != num_counts[i].0 + 1)
+            //     .unwrap_or(num_counts.len());
+
+            // Or use binary search
+            let next_pos = num_counts[i + 1..num_counts.len()]
+                .partition_point(|(num, _)| num_counts[i].0 + 1 >= *num) + i + 1;
+
+            let earn = num_counts[i].0 * num_counts[i].1;
+            dp[i] = dp[i].max(earn + dp[next_pos]);
         }
         dp[0]
     }
@@ -45,5 +66,13 @@ mod tests {
     #[test]
     fn test2() {
         assert_eq!(Solution::delete_and_earn(vec![2, 2, 3, 3, 3, 4]), 9);
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(
+            Solution::delete_and_earn(vec![1, 1, 1, 2, 4, 5, 5, 5, 6]),
+            18
+        );
     }
 }
