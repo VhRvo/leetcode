d3e87425cfb1d23c6076f7a6c6b7690f6cbdab93
VhRvo <zyzhuo@outlook.com>
2026-01-01 20:46:59 +0800

    start: 1970. Last Day Where You Can Still Cross

---
+++ src/lc1970.rs
@@ -0,0 +1,119 @@
+struct Solution;
+
+impl Solution {
+    pub fn latest_day_to_cross(row: i32, col: i32, cells: Vec<Vec<i32>>) -> i32 {
+        let row = row as usize;
+        let col = col as usize;
+        let mut connection = {
+            let mut connection = vec![vec![0; col + 2]; row + 2];
+            connection[1][1] = 2;
+            connection[1][col] = 2;
+            connection[row][1] = 2;
+            connection[row][col] = 2;
+            for ii in 2..row {
+                connection[ii][1] = 3;
+                connection[ii][col] = 3;
+            }
+            for jj in 2..col {
+                connection[1][jj] = 3;
+                connection[row][jj] = 3;
+            }
+            for ii in 2..row {
+                for jj in 2..col {
+                    connection[ii][jj] = 4;
+                }
+            }
+            connection
+        };
+        let mut row_count = vec![0; row + 2];
+        let mut result = 0;
+        'outer:
+        for cell in cells.iter() {
+            let x = cell[0] as usize;
+            let y = cell[1] as usize;
+            if connection[x][y] > 1 {
+                row_count[x] += 1;
+                if row_count[x] == col {
+                    break;
+                }
+            }
+            connection[x][y] = 0;
+            for (x_offset, y_offset) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let new_x = (x as isize + x_offset) as usize;
+                let new_y = (y as isize + y_offset) as usize;
+                if connection[new_x][new_y] == 0 {
+                    continue;
+                }
+                connection[new_x][new_y] -= 1;
+                if connection[new_x][new_y] == 0 {
+                    continue;
+                }
+                row_count[new_x] += 1;
+                if row_count[new_x] == col {
+                    break 'outer;
+                }
+            }
+            result += 1;
+        }
+
+        result
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            Solution::latest_day_to_cross(
+                2,
+                2,
+                [[1, 1], [2, 1], [1, 2], [2, 2]]
+                    .map(|row| row.to_vec())
+                    .to_vec()
+            ),
+            4
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            Solution::latest_day_to_cross(
+                2,
+                2,
+                [[1, 1], [1, 2], [2, 1], [2, 2]]
+                    .map(|row| row.to_vec())
+                    .to_vec()
+            ),
+            4
+        );
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(
+            Solution::latest_day_to_cross(
+                3,
+                3,
+                [
+                    [1, 2],
+                    [2, 1],
+                    [3, 3],
+                    [2, 2],
+                    [1, 1],
+                    [1, 3],
+                    [2, 3],
+                    [3, 2],
+                    [3, 1]
+                ]
+                .map(|row| row.to_vec())
+                .to_vec()
+            ),
+            4
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ mod lc15;
 mod lc1512;
 mod lc1518;
 mod lc1534;
+mod lc1970;
 mod lc16;
 mod lc167;
 mod lc1738;
