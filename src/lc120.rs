e2a5f94402d35a05c62b13e404db06ed7bed78bc
VhRvo <zyzhuo@outlook.com>
2026-01-03 20:21:40 +0800

    120. Triangle solved: optimize space.

--- src/lc120.rs
+++ src/lc120.rs
@@ -2,18 +2,13 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-        let rows = triangle.len();
-        let columns = triangle.last().unwrap().len();
-        let mut dp = vec![vec![0; columns]; 2];
-        dp[(rows - 1) % 2] = triangle.last().unwrap().clone();
-        for (ii, row) in triangle.iter().enumerate().rev().skip(1) {
-            let current = ii % 2;
-            let next = (ii + 1) % 2;
+        let mut dp = triangle.last().unwrap().clone();
+        for row in triangle.iter().rev().skip(1) {
             for (jj, element) in row.iter().enumerate() {
-                dp[current][jj] = dp[next][jj].min(dp[next][jj + 1]) + *element;
+                dp[jj] = dp[jj].min(dp[jj + 1]) + *element;
             }
         }
-        dp[0][0]
+        dp[0]
     }
 }
 
