c429e339dfb3cee3c2087756ae27de2b39ca36fa
VhRvo <zyzhuo@outlook.com>
2026-01-06 11:18:22 +0800

    1975. Maximum Matrix Sum solved: without sorting.

--- src/lc/lc1975.rs
+++ src/lc/lc1975.rs
@@ -3,7 +3,8 @@ struct Solution;
 impl Solution {
     pub fn max_matrix_sum(matrix: Vec<Vec<i32>>) -> i64 {
         let mut minimal_natural = i32::MAX;
-        let mut negatives = Vec::with_capacity(matrix.len());
+        let mut maximal_negative = i32::MIN;
+        let mut negative_count = 0;
         let mut result = 0 as i64;
         for row in matrix.iter() {
             for &element in row {
@@ -11,24 +12,16 @@ impl Solution {
                     minimal_natural = minimal_natural.min(element);
                     result += element as i64;
                 } else {
-                    negatives.push(-element);
+                    negative_count += 1;
+                    maximal_negative = maximal_negative.max(element);
+                    result -= element as i64;
                 }
             }
         }
-        negatives.sort();
-        let rest = if negatives.len() % 2 == 0 {
-            0
-        } else {
-            if minimal_natural < negatives[0] {
-                result += (negatives[0] - 2 * minimal_natural) as i64;
-            } else {
-                result -= negatives[0] as i64;
-            }
-            1
-        };
-        for &negative in negatives.iter().skip(rest) {
-            result += negative as i64;
+        if negative_count % 2 == 1 {
+            result -= 2 * ((-maximal_negative).min(minimal_natural) as i64);
         }
+
         result
     }
 }
