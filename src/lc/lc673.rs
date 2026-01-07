3d591f828db7904a6ff3ac4725a4dddb1a4f6dea
VhRvo <zyzhuo@outlook.com>
2026-01-07 13:47:53 +0800

    673. Number of Longest Increasing Subsequence solved: simple optimization.

--- src/lc/lc673.rs
+++ src/lc/lc673.rs
@@ -5,7 +5,7 @@ impl Solution {
         let mut dp_longest = vec![0; nums.len() + 1];
         let mut dp_count = vec![1; nums.len() + 1];
         let mut longest_length = 0;
-        let mut longest_indexes = Vec::new();
+        let mut result_count = 0;
 
         for ii in (0..nums.len()).rev() {
             let mut last_index = nums.len();
@@ -23,17 +23,13 @@ impl Solution {
             dp_longest[ii] = 1 + dp_longest[last_index];
             dp_count[ii] = count;
             if dp_longest[ii] == longest_length {
-                longest_indexes.push(ii)
+                result_count += count;
             } else if dp_longest[ii] > longest_length {
-                longest_indexes = vec![ii];
+                result_count = count;
                 longest_length = dp_longest[ii];
             }
         }
-        let mut result = 0;
-        for index in longest_indexes {
-            result += dp_count[index];
-        }
-        result
+        result_count
     }
 }
 
